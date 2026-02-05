098a6d802ed7bcf20e4d862800a0f1aa5873d7b3
tianze <3035582883@qq.com>
2026-02-05 15:44:37 +0800

    add

--- ws_livox/src/pointcloud_transform/launch/transform.launch.py
+++ ws_livox/src/pointcloud_transform/launch/transform.launch.py
@@ -5,16 +5,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 1. 静态 TF 发布 (lidar -> livox_frame, 旋转 -45 度)
-        # 如果你已经在别的地方启动了这个，可以注释掉下面这部分
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_tf_pub',
-            arguments=['0', '0', '0', '0', '0', '-0.785', 'lidar', 'livox_frame'],
-        ),
-
-        # 2. 点云转换节点
+        # 点云转换节点
         Node(
             package='pointcloud_transform',
             executable='transformer_node',
