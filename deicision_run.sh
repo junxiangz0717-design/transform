2a02a08a78ec80c52ca559b51928ce7b2bcb508d
yugaga <1169074503@qq.com>
2026-03-07 10:34:22 +0800

    add

--- deicision_run.sh
+++ deicision_run.sh
@@ -1,11 +1,15 @@
 gnome-terminal --tab --title="决策调参"  -- bash -c "wmctrl -r :ACTIVE: -b toggle,above; 
   source /opt/ros/jazzy/setup.bash; 
+  cd ~/transform/msg_process/;
+  source install/setup.bash
   cd ~/transform/Decision/;
   source install/setup.bash; 
   ros2 launch dummy_data_reconfigure param_tuning.launch.py;exec bash"
 
-gonme-terminal --tab --title="决策开始"  -- bash -c "wmctrl -r :ACTIVE: -b toggle,above; 
+gnome-terminal --tab --title="决策开始"  -- bash -c "wmctrl -r :ACTIVE: -b toggle,above; 
   source /opt/ros/jazzy/setup.bash; 
+  cd ~/transform/msg_process/;
+  source install/setup.bash
   cd ~/transform/Decision/;
   source install/setup.bash; 
-  ros2 run deision_process decision_process;exec bash"
+  ros2 run decision_process decision_process;exec bash"
